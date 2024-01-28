1eaf081973791e4a201bc942e521b3e9ae0516ed
Güven Boydak <80548551+GuvenBoydak@users.noreply.github.com>
2024-01-28 16:13:29 +0300

    Initial Commit

---
+++ Netflix_Clone_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Netflix_Clone_SwiftUI
+//
+//  Created by Güven Boydak on 28.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Netflix_Clone_SwiftUI/Netflix_Clone_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netflix_Clone_SwiftUIApp.swift
+//  Netflix_Clone_SwiftUI
+//
+//  Created by Güven Boydak on 28.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Netflix_Clone_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
